e335e9b94cf8e593ba31758bd8ef0f3154190ef4
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-09-08 16:35:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "8Y5nQaTQ#dgbSXvgOCQbmxwMLzt9hT5lnGBkRVR86KK-rcKfldUw",
-MONGODB: process.env.MONGODB || "mongodb+srv://dbUser:bXptn6Z!GD*5D37@cluster0.wserelz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+SESSION_ID: process.env.SESSION_ID || "PUT SESSION ID HERE!",
+MONGODB: process.env.MONGODB || "mongodb+srv://dbUser:bXptn6Z!GD*5D37@cluster0.wserelz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //ne pas changer
 };
