def0782e90ee9bcbcf15626e411d44f3d440edc0
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-09-08 10:47:57 +0100

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -4,7 +4,7 @@ const {cmd , commands} = require('../command')
 cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
-    category: "basic",
+    category: "basics",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
