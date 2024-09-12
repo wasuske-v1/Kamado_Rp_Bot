e47c3a5d560bd624681c3acba2adfb11ec2b6735
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-09-12 12:08:16 +0100

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -12,21 +12,40 @@ async (conn, mek, m, { from, reply, isOwner }) => {
 *🔶 LAUREAT'S CHANNEL📺*
 *_________*
 
-*👤Welcoming : Bien le bonjour, bonsoir, bonne nuit. Nous vous souhaitons la bienvenue dans la chaîne de LAUREAT, 🔶LAUREAT-TV📰. Installez-vous et suivez nos de très belles actualités sur le royaume de 🔶LAUREAT🎮 ainsi que les guerriers de la 🔶W.I.N🎮, << WARRIOR INFINITA NATION >>.*
-*Bien évidement nous n'allons pas nous limiter à notre RP au contraire nous sommes ouverts au monde extérieur également. Que ce soit RP ou pas tout va figurer ici.*
+*👤 Welcoming : Bien le bonjour, bonsoir, bonne nuit. Nous vous souhaitons la bienvenue dans la chaîne de LAUREAT, 🔶LAUREAT-TV📰. Installez-vous et suivez nos de très belles actualités sur le royaume de 🔶LAUREAT🎮 ainsi que les guerriers de la 🔶W.I.N🎮, << WARRIOR INFINITA NATION >>.*
+
+*Bien évidement nous n'allons pas nous limiter à notre RP, au contraire nous sommes ouverts au monde extérieur également. Que ce soit RP ou pas, tout va figurer ici.*
+
 *Je ne vous en dirai pas plus, alors accrochez-vous pour en découvrir plus ❗*
-*↪️JOIN :* https://chat.whatsapp.com/IntF7YkXGMZHR9d8Xqz1wO
+
 *_________*
 *@starkproduction🔸*
 *_________*
        *🔶 LAUREAT-TV📰*
 `;
 
+    const buttons = [
+        {
+            buttonId: 'join_channel',
+            buttonText: { displayText: '↪️ JOIN LAUREAT CHANNEL' },
+            type: 1,
+            urlButton: {
+                displayText: '↪️JOIN LAUREAT CHANNEL',
+                url: 'https://chat.whatsapp.com/IntF7YkXGMZHR9d8Xqz1wO',
+            },
+        },
+    ];
+
+    const buttonMessage = {
+        text: channelText,
+        footer: 'LAUREAT-TV',
+        buttons: buttons,
+        headerType: 1,
+    };
+
     try {
-        // Envoi du message formaté sans image
-        return await conn.sendMessage(from, { 
-            text: channelText 
-        }, { quoted: mek });
+        // Envoi du message avec le bouton URL
+        await conn.sendMessage(from, buttonMessage, { quoted: mek });
     } catch (err) {
         console.error("Erreur lors de l'envoi du canal LAUREAT:", err);
         reply("🙇‍♂️ Erreur lors de l'envoi du message. Réessaie plus tard.");
