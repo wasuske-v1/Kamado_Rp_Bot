c7d35626c61607a2e39173185db0c2203533fa1e
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-09-12 11:03:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -192,7 +192,6 @@ mek.type === "stickerMessage"
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
 //============================================================================ 
-if
         
 })
 }
