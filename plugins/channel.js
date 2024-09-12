b9f5063eaa6d72ce4124b4bf2716a4540a0e1db0
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-09-12 14:52:51 +0100

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -1,4 +1,5 @@
-const { proto, generateWAMessageFromContent, generateWAMessageContent } =  require('@whiskeysockets/baileys');
+
+const { proto, generateWAMessageFromContent, generateWAMessageContent } = require('@whiskeysockets/baileys');
 const { cmd } = require('../command');
 
 cmd({
@@ -7,7 +8,7 @@ cmd({
     category: "basics",
     filename: __filename,
 }, async (conn, mek, m, { from, reply }) => {
-
+    
     const generate = async (type, url) => {
         const generated = await generateWAMessageContent({
             [type]: { url }
@@ -26,8 +27,7 @@ cmd({
                 },
                 interactiveMessage: proto.Message.InteractiveMessage.create({
                     body: proto.Message.InteractiveMessage.Body.create({
-                        text: `
-                        *🔶 LAUREAT'S CHANNEL📺*
+                        text: `*🔶 LAUREAT'S CHANNEL📺*
                         *_________*
                         
                         *👤Welcoming : Bien le bonjour, bonsoir, bonne nuit. Nous vous souhaitons la bienvenue dans la chaîne de LAUREAT, 🔶LAUREAT-TV📰. Installez-vous et suivez nos de très belles actualités sur le royaume de 🔶LAUREAT🎮 ainsi que les guerriers de la 🔶W.I.N🎮, << WARRIOR INFINITA NATION >>.*
@@ -36,24 +36,23 @@ cmd({
                         *_________*
                         *@starkproduction🔸*
                         *_________*
-                               *🔶 LAUREAT-TV📰*
-                        `
+                               *🔶 LAUREAT-TV📰*`
                     }),
                     footer: proto.Message.InteractiveMessage.Footer.create({
                         text: "LAUREAT-TV - Suivez-nous pour plus d'informations !"
                     }),
                     header: proto.Message.InteractiveMessage.Header.create({
                         title: "LAUREAT-TV",
-                        hasMediaAttachment: true, // Si tu veux ajouter une image ou vidéo, mets true
-                         imageMessage: await generate("image", "https://i.imgur.com/9dH1Llt.jpeg") // Ajoute ici une image si nécessaire
+                        hasMediaAttachment: true, // Si tu veux ajouter une image ou vidéo
+                        imageMessage: await generate("image", "https://i.imgur.com/NBYjSbe.jpeg"), // Mets ici le chemin ou URL de l'image
                     }),
                     nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
                         buttons: [{
                             name: "cta_url",
                             buttonParamsJson: JSON.stringify({
-                                display_text: "JOIN", // Texte affiché sur le bouton
-                                url: "https://chat.whatsapp.com/FgnA1ANKadWE9FXilOCiWq", // URL du lien vers la chaîne WhatsApp
-                                merchant_url: "https://chat.whatsapp.com" // URL de redirection
+                                display_text: "Join!",
+                                url: "https://chat.whatsapp.com/IntF7YkXGMZHR9d8Xqz1wO", // URL de redirection
+                                merchant_url: "https://chat.whatsapp.com/IntF7YkXGMZHR9d8Xqz1wO"
                             })
                         }]
                     })
