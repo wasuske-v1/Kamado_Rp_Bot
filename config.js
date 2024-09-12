959e198d9ea9f9353c97d6135351332602c86578
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-09-12 11:14:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "8BJTQQRQ#7MvZgVk3wgsZ7wW7KRIiP9uhYfuxWz3szc0wDMaSiBU",
-MONGODB: process.env.MONGODB || "mongodb+srv://dbUser:bXptn6Z!GD*5D37@cluster0.wserelz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //ne pas changer
+MONGODB: process.env.MONGODB || "mongodb+srv://raizen:darki123@cluster0.utsln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //ne pas changer
 };
