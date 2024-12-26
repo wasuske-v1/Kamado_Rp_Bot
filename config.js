b18fc36351b4664adc11ef1b515eb40ba0327b2e
Laureat-commit <freefireneutre23@gmail.com>
2024-12-26 01:40:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gVZSHLpA#bXeYUszjdB_XSzJr3qDtlDvJ0TjXSzRLfiJuT7h2GIM", 
+SESSION_ID: process.env.SESSION_ID || "5ZgFUJQQ#N7RsQyvP8JevgFfma0Bq4XQTd5FzQ1ocZDLAKNHc7CU", 
 MONGODB: process.env.MONGODB || "mongodb+srv://raizen:darki123@cluster0.utsln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", //ne pas changer
 };
